c33fe3e5121c6a6536512250eae717415e78202d
ChartaP <charta97@naver.com>
2020-11-29 01:35:16 +0900

    소리 추가
    소리 추가
    소리 이벤트 리스너 추가

---
+++ Colosseum/Assets/Scripts/AudioCtrl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] attackClips;
+    [SerializeField]
+    private AudioClip[] hitClips;
+    [SerializeField]
+    private AudioClip[] footstepClips;
+    [SerializeField]
+    private AudioClip screamClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Die()
+    {
+        AudioSource.PlayClipAtPoint(screamClip, transform.position);
+    }
+
+    public void Attack()
+    {
+        AudioSource.PlayClipAtPoint(attackClips[Random.Range(0, attackClips.Length)], transform.position);
+    }
+
+    public void Hit()
+    {
+        AudioSource.PlayClipAtPoint(hitClips[Random.Range(0, hitClips.Length)], transform.position);
+    }
+
+    public void FootStep()
+    {
+        AudioSource.PlayClipAtPoint(footstepClips[Random.Range(0, footstepClips.Length)], transform.position);
+    }
+}
--- Colosseum/Assets/Scripts/CharCtrl.cs
+++ Colosseum/Assets/Scripts/CharCtrl.cs
@@ -169,4 +169,5 @@ public class CharCtrl : MonoBehaviourPun
             Destroy(nameTagTransform.gameObject);
         }
     }
+
 }
